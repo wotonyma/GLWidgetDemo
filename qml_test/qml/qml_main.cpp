1e5b81eb0686446823fd7c966b9673dbb66b28a8
colorfulPC <personalPC@colorful.com>
2024-07-16 21:45:26 +0800

    vcpkg baseline update and boost prf test

--- qml_test/qml/qml_main.cpp
+++ qml_test/qml/qml_main.cpp
@@ -5,6 +5,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "boost/pfr.hpp"
+#include <optional>
+#include <string>
+struct Tst {
+    int a;
+    float b;
+    char c;
+    std::optional<std::string> str;
+};
+
 int main(int argc, char* argv[])
 {
 	fmt::print("hello qml");
@@ -18,6 +28,9 @@ int main(int argc, char* argv[])
     //engine.load(":/main_ui.qml"); //ok ,上面那种写法必须要有前缀qrc
     if (engine.rootObjects().isEmpty())
         return -1;
+
+    Tst tst{1, 2.4f, 'y', "hello"};
+    auto i = boost::pfr::get<3>(tst);
 
     return app.exec();
 }
